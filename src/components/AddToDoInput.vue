f413db869e1ed0830639ca5e78618f11617c8f7a
Hammam Hussein <hamamhussein10@gmail.com>
2025-09-22 00:05:09 +0200

    update ui

--- src/components/AddToDoInput.vue
+++ src/components/AddToDoInput.vue
@@ -1,14 +1,14 @@
 <template>
-  <form @submit.prevent="handleAddNewTodo" class="flex gap-2 w-full">
+  <form @submit.prevent="handleAddNewTodo" class="md:flex gap-2 w-full">
     <input
       v-model="title"
       type="text"
       placeholder="Enter your task..."
-      class="md:flex-1 w-[150px] px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      class="md:flex-1 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
     <button
       type="submit"
-      class="px-4 py-2 text-xs text-nowrap bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      class="px-4 py-2 md:mt-0 mt-4 w-full text-xs text-nowrap bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
     >
       {{ isAddingTodo ? 'task adding...' : 'Add Task' }}
     </button>
